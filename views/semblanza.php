41bf6d5df1feee40a16263f1c795be1f018e73fd
Raúl Medina <raulmedinacampos@yahoo.com.mx>
2015-06-25 17:45:04 -0500

    Semblanza
    Se creo la vista de Semblanza del Ing. Marco Aurelio Torres H.

---
+++ views/semblanza.php
@@ -0,0 +1,28 @@
+<link type="text/css" rel="stylesheet" href="../css/interiores.css" />
+
+<div class="container" id="contenido">
+  <div class="col-sm-10 col-sm-offset-1">
+    <h1 class="titulo">Semblanza</h1>
+    <p class="text-center"><img src="../images/photo-semblance.png" width="402" height="295" alt=""/></p>
+    <h3 class="text-center text-uppercase dorado"><strong>Ing. Marco Aurelio Torres Herrera</strong></h3>
+    <div class="row">
+      <div class="col-sm-6">
+        <p>El ingeniero Marco Aurelio Torres Herrera es fundador y gran responsable de lo que es PUMITAS. Es profesor de la Facultad de Ingeniería desde el 16 de febrero de 1938; cumpliendo este año 65 años de servicios docentes ininterrumpidos. Actualmente es Profesor Titular &quot;C&quot; de Tiempo Completo en la División de Ciencias Sociales y Humanidades.</p>
+        <p>El ingeniero Marco Aurelio Torres Herrera es fundador y gran responsable de lo que es PUMITAS. Es profesor de la Facultad de Ingeniería desde el 16 de febrero de 1938;</p>
+        <p>Cumpliendo este año 65 años de servicios docentes ininterrumpidos. Actualmente es Profesor Titular &quot;C&quot; de Tiempo Completo en la División de Ciencias Sociales y Humanidades.</p>
+        <p>Ha participado en diversos cuerpos colegiados; fue consejero técnico de la Facultad durante 30 años (de 1952 a 1970, de 1975 a 1981 y de 1981 a 1987) y consejero universitario en tres ocasiones (de 1953 a 1957, de 1966 a 1970 y de 1983 a 1987). Junto con los profesores Manuel Paulín y Heberto Castillo, fundó la Unión de Profesores de la Facultad en 1953. Ha impartido las cátedras de Topografía, Estabilidad de las Construcciones, Mecánica Aplicada, Concreto Reforzado, Economía de la Empresa, Técnicas del Aprendizaje y la Disertación Cultura y Comunicación y Recursos y Necesidades de México.</p>
+        <p>Por acuerdo del Rector Javier Barros Sierra, fue miembro fundador de la actual División de Ciencias Sociales y Humanidades; es socio fundador del Colegio de Ingenieros Civiles de México.</p>
+        <p>Ha dictado un gran número de conferencias relacionadas con la ingeniería y las humanidades.</p>
+      </div>
+      <div class="col-sm-6">
+        <p>Organizó y difundió 964 programas de televisión en los canales 2, 4, 5, 7, 8, 11 y 13 así 368 programas de radio a través de Radio UNAM.</p>
+        <p>Impartió el Primer Curso de Autoconstrucción para el STUNAM, con la asistencia de 650 trabajadores.</p>
+        <p>En cuanto a su producción editorial, el Ing. Torres Herrera ha publicado apuntes sobre Estática, Economía de la Empresa, Técnicas del Aprendizaje y la Disertación y Recursos y Necesidades de México. Colaboró en la edición del Manual de Autoconstrucción, publicó diversos artículos en el periódico &quot;El Nacional&quot; y es autor de dos libros de texto sobre Concreto Reforzado: &quot;Teoría Elástica&quot; y &quot;Teoría Plástica&quot;.</p>
+        <p>Investigó y produjo un nuevo material para construcción denominado adobecemento; publicado en la tercera edición del Manual de autoconstrucción. Es Presidente de la Comisión Dictaminadora de la División de Ciencias Sociales y Humanidades desde 1974.</p>
+        <p>Fue activo promotor de la fundación de los Colegios del Personal Académico de la Facultad y actualmente es Presidente del Colegio del Personal Académico de la División de Ciencias Sociales y Humanidades.</p>
+        <p>Fundó en 1976 la Organización Pumitas A.C. y fue su presidente activo hasta julio de  2002.</p>
+        <p>Fue designado Profesor Emérito de la Facultad de Ingeniería, el 8 de diciembre de 1989, en la que actualmente es Coordinador de los &quot;Seminarios de Superación Académica&quot; de la División de Ciencias Sociales y Humanidades.</p>
+      </div>
+    </div>
+  </div>
+</div>
