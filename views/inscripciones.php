d9765fde8a8a2904eedb7c8ab09f702076585a08
Raúl Medina <raulmedinacampos@yahoo.com.mx>
2015-06-25 17:10:07 -0500

    Inscripciones
    Se creo la vista de Inscripciones

--- views/inscripciones.php
+++ views/inscripciones.php
@@ -1,8 +1,56 @@
+<link type="text/css" rel="stylesheet" href="../css/interiores.css" />
+
 <div class="container" id="contenido">
-  <h1>Inscripciones</h1>
-  <p>Nuevo ingreso 2015</p>
-  <p>Estimados padres de familia interesados en inscribir a sus hijos(as) en la Organización Pumitas C.U. Futbol A.C.:</p>
-  <p>En virtud de que contamos con espacios limitados, informamos a ustedes que no podremos dar lugar a todos los solicitantes. El procedimiento para la inscripción del día que corresponda será:</p>
-  <p>1.- Solo se tramitarán solicitudes a las personas con la documentación completa y que se presenten con el niño o niña que van a inscribir, la entrega de la solicitud no garantiza el lugar.</p>
-  <p>2.- La documentación es:</p>
+  <div class="col-sm-8 col-sm-offset-2">
+    <h1 class="titulo">Inscripciones</h1>
+    <h4 class="text-uppercase"><strong>Nuevo ingreso 2015</strong></h4>
+    <p>Estimados padres de familia interesados en inscribir a sus hijos(as) en la Organización Pumitas C.U. Futbol A.C.:</p>
+    <p>En virtud de que contamos con espacios limitados, informamos a ustedes que no podremos dar lugar a todos los solicitantes. El procedimiento para la inscripción del día que corresponda será:</p>
+    <p>1.- Solo se tramitarán solicitudes a las personas con la documentación completa y que se presenten con el niño o niña que van a inscribir, la entrega de la solicitud no garantiza el lugar.</p>
+    <p>2.- La documentación es:</p>
+    <div class="row requisitos">
+      <div class="col-sm-6">
+        <p><span class="text-uppercase dorado">Personal académico no definitivo, <br>
+          empleados UNAM y egresados:</span><br />
+          (original y copia)
+		</p>
+        <ul>
+          <li>Identificacion del papá o la mamá universitaria</li>
+          <li>Último talón de cheque UNAM o título</li>
+          <li>Acta de nacimiento del niño(a)</li>
+          <li>Comprobante de domicilio</li>
+          <li>2 fotos tamaño credencial o infantil</li>
+          <li>Constancia de seguro de gastos médicos (IMSS, ISSSTE, privado)</li>
+        </ul>
+      </div>
+      <div class="col-sm-6">
+        <p><span class="text-uppercase dorado">Personal académico con definitividad UNAM</span><br />
+          (original y copia)
+		</p>
+        <ul>
+          <li>Carta de definitividad emitida por los consejos técnicos respectivos</li>
+          <li>Credencial UNAM</li>
+          <li>Último talón de cheque UNAM</li>
+          <li>Acta de nacimiento del niño(a)</li>
+          <li>Comprobante de domicilio</li>
+          <li>2 fotos tamaño credencial o infantil</li>
+          <li>Constancia de seguro de gastos médicos (IMSS, ISSSTE, privado)</li>
+        </ul>
+      </div>
+    </div>
+    <p>3.- Una vez llenada la solicitud, revisada la documentación y que se pese y mida al niño, el papá o la mamá debe acudir a la junta informativa que se realiza en el &quot;Auditorio Marco A. Torres H.&quot; en el día y hora que se les indique. No es necesario que los hijos asistan a la junta, pero sí es obligatoria la asistencia de al menos uno de los padres.</p>
+    <p>4.- Se entregarán fichas de pago a las personas que cumplan al 100% con los requisitos y se cuente con lugar seguro en la categoría que corresponda a su hijo. Deberán depositar antes de 15 días a partir de la entrega. Deberán presentarse el siguiente sábado o a más tardar en 15 días después, sólo en sábados, en la oficina de inscripciones en horario de 9:00 a 11:00 horas con original y copia del depósito. La fecha límite para presentar el depósito es dentro de dos sábados (15 días). Después de esa fecha quedará condicionado su lugar, a que haya espacio en los equipos, aunque hayan efectuado el depósito con anterioridad, en ese caso se reembolsará el mismo.</p>
+    <p>5.- No se entregarán fichas de pago cuando no se tenga lugar asegurado. Se les mantendrá en lista de espera.</p>
+    <p>El personal académico con definitividad, así como las niñas tienen cuotas presenciales en base al convenio que se tiene con la UNAM.</p>
+    <p>Para fechas de inscripciones llamar al 5611 6589 de lunes a viernes de 10 a 16 horas.</p>
+    <p>La limitación de los espacios en los equipos se debe a lo siguiente:<br />
+    La UNAM nos ha asignado horarios y un área limitada debido a que el uso de las instalaciones es compartido con estudiantes, profesores, egresados y personal administrativo de la UNAM.</p>
+    <p>Debido al perfil de nuestros monitores, el personal técnico disponible que garantice mantener la calidad en el servicio que prestamos también es limitado.</p>
+    <p>Aprovechamos para informar que no tenemos una relación directa con el club de futbol UNAM, aunque mantenemos una relación estrecha y de apoyo mutuo.</p>
+    <p><em>Favor de leer el folleto informativo respectivo.</em></p>
+    <p class="firma"><em>Agradecemos su comprensión y paciencia.</em></p>
+    <p class="firma"><em>Atentamente.</em><br />
+      <em>&quot;Convivir más que competir.&quot;</em><br />
+    <em class="dorado">Pumitas C.U. Futbol A.C.T.</em></p>
+  </div>
 </div>
